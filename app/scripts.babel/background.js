87c726538841691994c0cd5bf179a25c78939f36
MunKy <maxi.techerag@gmail.com>
2016-04-22 01:44:23 -0300

    addded content scripts

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,9 +4,9 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript(null, {file: "jquery.min.js"}, () => {
+  chrome.tabs.executeScript(null, {file: 'jquery.min.js'}, () => {
     console.log('\'Allo \'Jquery loaded');
-    chrome.tabs.executeScript(null, {file: "background.js"}, ()=>{
+    chrome.tabs.executeScript(null, {file: 'background.js'}, ()=>{
       console.log('\'Allo \'Jquery loaded');
     });
   });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -5,17 +5,31 @@
 var inputFile = document.getElementById('file');
 var inputOpacityFilter = document.getElementById('opacity');
 var spanOpacityValue = document.getElementById('opacity-value');
+var media = $('.holder-selected-media');
 var overlay;
 var opacityValue = 1;
+var $preview = $('#preview-media');
 inputFile.addEventListener('change', (event)=> {
   if (inputFile.files && inputFile.files.length > 0) {
     let file = inputFile.files[0];
-    console.log(file);
+    var reader = new FileReader();
+    reader.onload = (e)=> {
+      $preview.attr('src', e.target.result);
+      console.log($('body'), document);
+      $('body').append($preview.clone());
+    };
+    reader.readAsDataURL(file);
   }
 });
 
-inputOpacityFilter.addEventListener('change', (event)=> {
+inputOpacityFilter.addEventListener('change', opacityValueChanged);
+inputOpacityFilter.addEventListener('input', opacityValueChanged);
+
+
+function opacityValueChanged(event) {
   //TODO get the input value and set opacity to the overlay
-  opacityValue = inputFile.value();
+  opacityValue = $(inputOpacityFilter).val();
   spanOpacityValue.innerHTML = opacityValue;
-});
+  media.css({opacity: opacityValue / 100})
+
+}
